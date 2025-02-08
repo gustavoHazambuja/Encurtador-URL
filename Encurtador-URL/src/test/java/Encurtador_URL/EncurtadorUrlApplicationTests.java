7795cd0d048ac7a490fa8193d7daffdf1435e66c
Gustavo Henrique <guazambujinha@gmail.com>
2025-02-08 16:01:26 -0300

    fist commit

---
+++ Encurtador-URL/src/test/java/Encurtador_URL/EncurtadorUrlApplicationTests.java
@@ -0,0 +1,13 @@
+package Encurtador_URL;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EncurtadorUrlApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
